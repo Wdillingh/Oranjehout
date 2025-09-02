57249f70a7e02f24b7bf037d54a7f0199853cc01
Wdillingh <164799122+Wdillingh@users.noreply.github.com>
2025-09-02 19:51:21 +0200

    Update main.js
    script lightbox toegevoegd.

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,5 +119,28 @@
 					target: $body,
 					visibleClass: 'header-visible'
 				});
+// Lightbox functionaliteit
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const galleryImages = document.querySelectorAll('.gallery img');
+const closeBtn = document.querySelector('.lightbox .close');
+
+galleryImages.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+});
+
 
 })(jQuery);
